63fbcfd17b0a997f338e874a5080355a743cb449
phonhay103 <49369395+phonhay103@users.noreply.github.com>
2021-06-01 10:02:45 +0700

    Report

--- app.py
+++ app.py
@@ -20,44 +20,27 @@ def load_style_image(img, image_size=(256, 256)):
 
 def load_content_image(img):
     img = img / 255.
-    if img.shape[0] > 1080:
-        img = tf.image.resize(img, (1080, 1080), preserve_aspect_ratio=True)
-    else:
-        img = tf.convert_to_tensor(img, dtype=tf.float32)
+    # if img.shape[0] > 1080:
+    #     img = tf.image.resize(img, (1080, 1080), preserve_aspect_ratio=True)
+    # else:
+    #     img = tf.convert_to_tensor(img, dtype=tf.float32)
+    img = tf.convert_to_tensor(img, dtype=tf.float32)
     img = tf.expand_dims(img, axis=0)
     return img
 
 @st.cache
 def load_model():
-    hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
-    hub_module = hub.load(hub_handle)
-    return hub_module
+    # return hub.load('magenta_arbitrary-image-stylization-v1-256_2/')
+    return hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 ### Function ###
 
+model = load_model()
+
 ### GUI ###
-st.title('My app')
+st.title('Art Work')
 
 # Model
 st.sidebar.selectbox("Model", ['TF-HUB', 'Model 1', 'Model 2'])
-
-# Style Image
-style_list_path = os.listdir('images/style/')
-style_list = [s.split('.')[0].replace('_', ' ').title() for s in style_list_path]
-style_list.append('Custom')
-style = st.sidebar.selectbox("Image Style: ", style_list)
-if style == 'Custom':
-    style_custom = st.sidebar.file_uploader('Style image')
-    if style_custom:
-        style_img = load_image(style_custom)
-        st.header("Style Image")
-        st.image(style_img)
-        style_img = load_style_image(style_img)
-else:
-    style_image_path = 'images/style/' + style_list_path[style_list.index(style)]
-    style_img = load_image(style_image_path)
-    st.header("Style Image")
-    st.image(style_img)
-    style_img = load_style_image(style_img)
 
 # Content Image
 content = st.file_uploader('IMAGES TO PROCESS', type=['JPG', 'PNG'])
@@ -66,6 +49,33 @@ if content:
     content_img = load_image(content)
     st.image(content_img)
     content_img = load_content_image(content_img)
+
+# Style Image
+style_list_path = os.listdir('images/style/')
+style_list = [s.split('.')[0].replace('_', ' ').title() for s in style_list_path]
+style_list.append('Custom')
+style_list.append('One For All')
+style = st.sidebar.selectbox("Image Style: ", style_list)
+all_styles = []
+if style == 'Custom':
+    style_custom = st.sidebar.file_uploader('Style image', type=['JPG', 'PNG'])
+    if style_custom:
+        style_img = load_image(style_custom)
+        st.header("Style Image")
+        st.image(style_img)
+        style_img = load_style_image(style_img)
+elif style == 'One For All':
+    for style_index in range(len(style_list)-2):
+        style_image_path = 'images/style/' + style_list_path[style_index]
+        style_img = load_image(style_image_path)
+        style_img = load_style_image(style_img)
+        all_styles.append(style_img)
+else:
+    style_image_path = 'images/style/' + style_list_path[style_list.index(style)]
+    style_img = load_image(style_image_path)
+    st.header("Style Image")
+    st.image(style_img)
+    style_img = load_style_image(style_img)
 
 # Resolution
 resolution_type = ['Original', 'Custom', 'HD 16:9', 'Widescreen 16:10', 'Standard 4:3', 'Standard 5:4', 
@@ -100,12 +110,20 @@ if content and out_format == 'AUTO':
 
 # Button
 if st.sidebar.button("Start Processing") and content:
-    model = load_model()
-    stylized_image = model(tf.constant(content_img), tf.constant(style_img))[0][0]
-    if size:
-        stylized_image = tf.image.resize(stylized_image, size[::-1])
-    show_img = tf.keras.preprocessing.image.array_to_img(stylized_image)
-    st.header("Stylized Image")
-    st.image(show_img)
-    # stylized_image = tf.image.resize(stylized_image, size[::-1])
-    # tf.keras.preprocessing.image.save_img(f'images/stylized/{file_name}.{out_format}', stylized_image)
+    if len(all_styles) == 0:
+        stylized_image = model(tf.constant(content_img), tf.constant(style_img))[0][0]
+        if size:
+            stylized_image = tf.image.resize(stylized_image, size[::-1])
+        tf.keras.preprocessing.image.save_img(f'images/stylized/{file_name}.{out_format}', stylized_image)
+        show_img = tf.keras.preprocessing.image.array_to_img(stylized_image)
+        st.header("Stylized Image")
+        st.image(show_img)
+    else:
+        for i, style in enumerate(all_styles):
+            stylized_image = model(tf.constant(content_img), tf.constant(style))[0][0]
+            if size:
+                stylized_image = tf.image.resize(stylized_image, size[::-1])
+            tf.keras.preprocessing.image.save_img(f'images/stylized/{file_name} - {style_list[i]}.{out_format.lower()}', stylized_image)
+        show_img = tf.keras.preprocessing.image.array_to_img(stylized_image)
+        st.header("Stylized Image")
+        st.image(show_img)
